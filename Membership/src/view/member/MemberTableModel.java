f94d84b6c2f77a2ddbec13e3cc3518e64d2d06e1
joofrederik <121954413+joofrederik@users.noreply.github.com>
2024-11-10 19:29:23 +0700

    Update MemberTableModel.java

--- Membership/src/view/member/MemberTableModel.java
+++ Membership/src/view/member/MemberTableModel.java
@@ -1,49 +1,74 @@
-package view.member;  
+package view.member;
 
-import javax.swing.table.*;  
-import java.util.List;  
-import model.Member;  
+import javax.swing.table.*;
+import java.util.List;
+import model.Member;
 
-class MemberTableModel extends AbstractTableModel {  
-    private String[] columnNames = {"Nama", "Jenis Member"};  
-    private List<Member> data;  
+class MemberTableModel extends AbstractTableModel {
+    private String[] columnNames = {"Nama", "Jenis Member"};
+    private List<Member> data;
 
-    public MemberTableModel(List<Member> data) {  
-        this.data = data;  
-    }  
+    public MemberTableModel(List<Member> data) {
+        this.data = data;
+    }
 
-    public int getColumnCount() {  
-        return columnNames.length;  
-    }  
 
-    public int getRowCount() {  
-        return data.size();  
-    }  
+    @Override
+    public int getColumnCount() {
+        return columnNames.length;
+    }
 
-    public String getColumnName(int col) {  
-        return columnNames[col];  
-    }  
+    @Override
+    public int getRowCount() {
+        return data.size();
+    }
 
-    public Object getValueAt(int row, int col) {  
-        Member rowItem = data.get(row);  
-        String value = "";  
-        switch (col) {  
+    @Override
+    public String getColumnName(int col) {
+        return columnNames[col];
+    }
+
+    @Override
+    public Object getValueAt(int row, int col) {
+        Member rowItem = data.get(row);
+        if (rowItem == null) return "";
+        
+        switch (col) {
             case 0:  
-                value = rowItem.getNama();  
-                break;  
+                return rowItem.getNama();
             case 1:  
-                value = rowItem.getJenisMember().getNama();  
-                break;  
-        }  
-        return value;  
-    }  
+                return rowItem.getJenisMember() != null ? rowItem.getJenisMember().getNama() : "No Type";  // Handle null JenisMember
+            default: 
+                return null;
+        }
+    }
 
-    public boolean isCellEditable(int row, int col) {  
-        return false;  
-    }  
+    @Override
+    public boolean isCellEditable(int row, int col) {
+        return false; 
+    }
 
-    public void add(Member value) {  
-        data.add(value);  
-        fireTableRowsInserted(data.size() - 1, data.size() - 1);  
-    }  
-}
+   
+    public void add(Member value) {
+        data.add(value);
+        fireTableRowsInserted(data.size() - 1, data.size() - 1);
+    }
+
+    
+    public void remove(Member value) {
+        int row = data.indexOf(value);
+        if (row != -1) {
+            data.remove(row);
+            fireTableRowsDeleted(row, row);
+        }
+    }       
+    public void update(Member value) {
+            int rowIndex = data.indexOf(value);
+            if (rowIndex != -1) {
+                data.set(rowIndex, value);
+                fireTableRowsUpdated(rowIndex, rowIndex);
+            }
+        }
+        
+    }
+
